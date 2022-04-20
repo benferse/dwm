8a02557c85ff69f88ac0db1d64e9fe83f7deacab
Ben Fersenheim <ben@bu.edu>
2022-04-19 20:38:15 -0400

    Updates for resources

--- config.def.h
+++ config.def.h
@@ -3,15 +3,15 @@
 /* appearance */
 static       unsigned int borderpx  = 5;        /* border pixel of windows */
 static       unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static       unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static       unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static       unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static       unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static       int showbar            = 1;        /* 0 means no bar */
 static       int topbar             = 1;        /* 0 means bottom bar */
-static       char font[]            = "monospace:Regular:size=14:antialias=true:autohint=true";
-static       char dmenufont[]       = "monospace:Regular:size=14:antialias=true:autohint=true";
+static       char font[]            = "monospace:Regular:pixelsize=32:antialias=true:autohint=true";
+static       char dmenufont[]       = "monospace:Regular:pixelsize=32:antialias=true:autohint=true";
 static const char *fonts[]          = { font };
 static       char normbgcolor[]           = "#222222";
 static       char normbordercolor[]       = "#444444";
@@ -89,21 +89,25 @@ static const char *lockcmd[]  = { "slock", NULL };
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-		{ "font",               STRING,  &font },
-		{ "dmenufont",          STRING,  &dmenufont },
-		{ "normbgcolor",        STRING,  &normbgcolor },
-		{ "normbordercolor",    STRING,  &normbordercolor },
-		{ "normfgcolor",        STRING,  &normfgcolor },
-		{ "selbgcolor",         STRING,  &selbgcolor },
-		{ "selbordercolor",     STRING,  &selbordercolor },
-		{ "selfgcolor",         STRING,  &selfgcolor },
-		{ "borderpx",          	INTEGER, &borderpx },
-		{ "snap",          		INTEGER, &snap },
-		{ "showbar",          	INTEGER, &showbar },
-		{ "topbar",          	INTEGER, &topbar },
-		{ "nmaster",          	INTEGER, &nmaster },
-		{ "resizehints",       	INTEGER, &resizehints },
-		{ "mfact",      	 	FLOAT,   &mfact },
+    { "font",               STRING,  &font },
+    { "dmenufont",          STRING,  &dmenufont },
+    { "normbgcolor",        STRING,  &normbgcolor },
+    { "normbordercolor",    STRING,  &normbordercolor },
+    { "normfgcolor",        STRING,  &normfgcolor },
+    { "selbgcolor",         STRING,  &selbgcolor },
+    { "selbordercolor",     STRING,  &selbordercolor },
+    { "selfgcolor",         STRING,  &selfgcolor },
+    { "borderpx",           INTEGER, &borderpx },
+    { "snap",               INTEGER, &snap },
+    { "showbar",            INTEGER, &showbar },
+    { "topbar",             INTEGER, &topbar },
+    { "nmaster",            INTEGER, &nmaster },
+    { "resizehints",        INTEGER, &resizehints },
+    { "mfact",              FLOAT,   &mfact },
+    { "gappih",             INTEGER, &gappih },
+    { "gappiv",             INTEGER, &gappiv },
+    { "gappoh",             INTEGER, &gappoh },
+    { "gappov",             INTEGER, &gappov },
 };
 
 static Key keys[] = {
