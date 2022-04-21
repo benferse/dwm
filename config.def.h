1956020ad0ad862e8137b69b541bc1430ea3fb8b
Ben Fersenheim <benferse@microsoft.com>
2022-04-21 13:13:32 -0400

    Updates for tag rules

--- config.def.h
+++ config.def.h
@@ -34,7 +34,8 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    { "Firefox",  NULL,       NULL,       1 << 3,       0,           -1 },
+    {  "microsoft-edge-dev", NULL, NULL,  1 << 1,       0,           -1 },
+    {  NULL,      NULL,       "Firefox",  1 << 3,       0,           -1 },
 };
 
 /* layout(s) */
